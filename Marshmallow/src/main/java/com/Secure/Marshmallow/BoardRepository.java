7d87984c322b581907fe6417de6f60f6595d6860
Dowonjune <cantonetab0306@gmail.com>
2023-03-13 09:23:56 +0900

    🌊:: 보안 코드 추가(주석처리)

--- Marshmallow/src/main/java/com/Secure/Marshmallow/BoardRepository.java
+++ Marshmallow/src/main/java/com/Secure/Marshmallow/BoardRepository.java
@@ -9,11 +9,17 @@ public class BoardRepository {
     public Board writePost(String title, String writer, String content, String password) {
         String query = "INSERT INTO Board(title,writer,content,password) VALUES('" + title + "','" + writer + "','" + content + "','" + password + "')";
         return (Board) jdbcTemplate.queryForObject(query, new Object[]{title, writer,content,password}, new BeanPropertyRowMapper<>(User.class));
+        //String query = "INSERT INTO Board(title,writer,content,password) VALUES(?,?,?,?)";
+        //jdbcTemplate.update(query, new Object[]{title, writer, content, password});
+
     }
 
     public Board viewPost(int idx) {
         String query = "SELECT * FROM Board where idx='" + idx + "'";
         return (Board) jdbcTemplate.queryForObject(query, new Object[]{idx}, new BeanPropertyRowMapper<>(User.class));
-    }
+        //String query = "SELECT * FROM Board WHERE idx=?";
+        //return jdbcTemplate.queryForObject(query, new Object[]{idx}, new BeanPropertyRowMapper<>(Board.class));
 
+    }
+    //UserRepository와 마찬가지로 예외처리 추가 예정
 }
--- Marshmallow/src/main/java/com/Secure/Marshmallow/UserRepository.java
+++ Marshmallow/src/main/java/com/Secure/Marshmallow/UserRepository.java
@@ -5,6 +5,9 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
 @Repository
 public class UserRepository {
 
@@ -13,10 +16,18 @@ public class UserRepository {
     public User Login(String username, String password) {
         String query = "SELECT * FROM user WHERE username = '" + username + "' AND password = '" + password + "'";
         return jdbcTemplate.queryForObject(query, new Object[]{username, password}, new BeanPropertyRowMapper<>(User.class));
+        //String query = "SELECT * FROM user WHERE username = ? AND password = ?";
+        //return jdbcTemplate.queryForObject(query, new Object[]{username, password}, new BeanPropertyRowMapper<>(User.class));
+
     }
 
     public User Register(String username, String password, String id, String gmail) {
         String query = "INSERT INTO user(id,username,password,gmail) VALUES('"+ id +"','"+ username + "','" + password + "','" + gmail + "')";
         return jdbcTemplate.queryForObject(query, new Object[]{username, password, id, gmail}, new BeanPropertyRowMapper<>(User.class));
+        //String query = "INSERT INTO user(id,username,password,gmail) VALUES(?,?,?,?)";
+        //jdbcTemplate.update(query, new Object[]{id, username, password, gmail});
+
     }
+
+    // 코드들 예외처리 추가 예정
 }
